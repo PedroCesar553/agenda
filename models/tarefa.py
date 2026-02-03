7b65002c774c47b4f3f0df1c547d9f456587c6a6
PedroCesar553 <p.onofre@aluno.senai.br>
2026-02-03 16:45:26 -0300

    Excluindo Tarefas

--- models/tarefa.py
+++ models/tarefa.py
@@ -1,14 +1,42 @@
-from typing import Self
+from typing import Self, Any
 from models.database import Database
+from sqlite3 import Cursor
 
 class Tarefa:
-    def __init__(self: Self, titulo_tarefa: str, data_conclusao: str = None) -> None:
+    def __init__(self: Self, titulo_tarefa: str, data_conclusao: str = None, id_tarefa: int = None)-> None:
         self.titulo_tarefa: str = titulo_tarefa
-        self.data_conclusao:str = data_conclusao
+        self.data_conclusao: str  = data_conclusao 
+        self.id_tarefa: int = id_tarefa
 
-    def salvar_tarefa(self: Self) -> None:
+    @classmethod
+    def id(cls, id: int):
         with Database('./data/tarefas.sqlite3') as db:
-            query: str = "INSERT INTO tarefas (titulo_tarefa, data_conclusao) VALUES (?, ?);"
+         query: str = 'SELECT titulo_tarefa, data_conclusao FROM tarefas WHERE id = ?;'
+         params: tuple = (id,)
+         resultado = db.buscar_tudo(query, params)
+         print(resultado)
+
+         [[titulo, data]] = resultado
+         return cls(id_tarefa=id, titulo_tarefa=titulo, data_conclusao=data)
+        
+    def salvar_tarefa(self: Self)-> None:
+        with Database('./data/tarefas.sqlite3') as db:
+            query: str = " INSERT INTO tarefas (titulo_tarefa, data_conclusao) VALUES (?, ?);"
             params: tuple = (self.titulo_tarefa, self.data_conclusao)
             db.executar(query, params)
-    
+
+    @staticmethod
+    def obter_tarefas() -> list[Self]:
+        with Database('./data/tarefas.sqlite3') as db:
+            query: str = 'SELECT titulo_tarefa, data_conclusao FROM tarefas;'
+            resultados: list[Any] = db.buscar_tudo(query)
+            tarefas: list[Any] = [Tarefa(titulo, data) for titulo, data in resultados]
+        return tarefas
+        
+    def excluir_tarefa(self) -> Cursor:
+        with Database('./data/tarefas.sqlite3') as db:
+            query: str = "DELETE FROM tarefas WHERE id = ?;"
+            params: tuple = (self.id_tarefa,)
+            db.executar(query, params)
+            resultado: Cursor = db.executar(query, params)
+        return resultado
